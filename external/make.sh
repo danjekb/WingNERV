b38249cba65c79fd1922f558cd351fa2151bd838
Tjari <zadigsm@outlook.com>
2026-04-19 08:07:55 +0700

    external/magiskboot: nuke

--- external/make.sh
+++ external/make.sh
@@ -132,7 +132,6 @@ ANDROID_TOOLS=true
 APKTOOL=true
 EROFS_UTILS=true
 IMG2SDAT=true
-MAGISKBOOT=true
 SAMFWDL=true
 SIGNAPK=true
 
@@ -157,10 +156,6 @@ IMG2SDAT_EXEC=(
     "blockimgdiff.py" "common.py" "images.py" "img2sdat" "rangelib.py" "sparse_img.py"
 )
 CHECK_TOOLS "${IMG2SDAT_EXEC[@]}" && IMG2SDAT=false
-MAGISKBOOT_EXEC=(
-    "magiskboot"
-)
-CHECK_TOOLS "${MAGISKBOOT_EXEC[@]}" && MAGISKBOOT=false
 SAMFWDL_EXEC=(
     "../venv/bin/samfwdl"
 )
@@ -175,7 +170,6 @@ if [[ "$1" == "--check-tools" ]]; then
             ! $APKTOOL && \
             ! $EROFS_UTILS && \
             ! $IMG2SDAT && \
-            ! $MAGISKBOOT && \
             ! $SAMFWDL && \
             ! $SIGNAPK; then
         exit 0
@@ -238,26 +232,6 @@ if $IMG2SDAT; then
     )
 
     BUILD "img2sdat" "$SRC_DIR/external/img2sdat" "${IMG2SDAT_CMDS[@]}"
-fi
-if $MAGISKBOOT; then
-    case "$(uname -m)" in
-        arm64|aarch64)
-            ARCH="arm64-v8a"
-            ;;
-        amd64|x86_64)
-            ARCH="x86_64"
-            ;;
-    esac
-    MAGISKBOOT_TMP="$(mktemp -d)"
-    MAGISKBOOT_CMDS=(
-        "curl -L -s -o \"magisk.apk\" \"$(curl -s https://api.github.com/repos/topjohnwu/Magisk/releases/latest | jq -r ".assets[] | .browser_download_url" | grep "Magisk.*.apk")\""
-        "unzip -q -j \"magisk.apk\" \"lib/$ARCH/libmagiskboot.so\""
-        "mv \"libmagiskboot.so\" \"$TOOLS_DIR/bin/magiskboot\""
-        "chmod +x \"$TOOLS_DIR/bin/magiskboot\""
-    )
-
-    BUILD "magiskboot" "$MAGISKBOOT_TMP" "${MAGISKBOOT_CMDS[@]}"
-    rm -rf "$MAGISKBOOT_TMP"
 fi
 if $SAMFWDL; then
     SAMFWDL_CMDS=(
