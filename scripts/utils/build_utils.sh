515ae47057cce092e9a98176f725d2c6f05337c7
Tjari <zadigsm@outlook.com>
2026-04-19 15:34:28 +0700

    scripts: utils: build_utils: nuke unused dependencies

--- scripts/utils/build_utils.sh
+++ scripts/utils/build_utils.sh
@@ -108,11 +108,11 @@ READ_BYTES_AT()
 DEPENDENCIES=(
     "awk" "basename" "bc" "brotli" "cat" "clang" "cmake"
     "cp" "curl" "cut" "cwebp" "dd" "dirname" "du" "ffmpeg"
-    "file" "getfattr" "git" "go" "grep" "head" "java" "ln"
-    "lz4" "make" "md5sum" "mkdir" "mount" "mv" "pcre2test"
-    "perl" "protoc" "python3" "rm" "sed" "sha1sum" "sort"
-    "stat" "tail" "tar" "touch" "tr" "truncate" "umount"
-    "unzip" "wc" "whoami" "xargs" "xxd" "zip" "zstd"
+    "file" "getfattr" "git" "grep" "head" "java" "ln" "lz4"
+    "make" "md5sum" "mkdir" "mount" "mv" "perl" "protoc"
+    "python3" "rm" "sed" "sha1sum" "sort" "stat" "tail"
+    "tar" "touch" "tr" "truncate" "umount" "unzip" "wc"
+    "whoami" "xargs" "xxd" "zip" "zstd"
 )
 MISSING=()
 for d in "${DEPENDENCIES[@]}"; do
