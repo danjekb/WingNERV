c6cf6eabdc8ea0f74d1ad94a4ee1137fac64caab
Tjari <zadigsm@outlook.com>
2026-04-20 22:04:58 +0700

    unica: patches: vndk: add VNDK 30, 31, 33 apex

--- unica/patches/vndk/customize.sh
+++ unica/patches/vndk/customize.sh
@@ -8,7 +8,7 @@ ADD_TARGET_VNDK_APEX() {
             ADD_TO_WORK_DIR "r0qxxx" "system_ext" "apex/com.android.vndk.v31.apex" 0 0 644 "u:object_r:system_file:s0"
             ;;
         "33")
-            ADD_TO_WORK_DIR "dm3qxxx" "system_ext" "apex/com.android.vndk.v33.apex" 0 0 644 "u:object_r:system_file:s0"
+            ADD_TO_WORK_DIR "a05snsdxx" "system_ext" "apex/com.android.vndk.v33.apex" 0 0 644 "u:object_r:system_file:s0"
             ;;
         *)
             ABORT "No APEX blob available for VNDK $TARGET_VENDOR_API_LEVEL"
