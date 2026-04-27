12022c9c19de4fa86e44f1c135cf71b6535f6ac6
Tjari <zadigsm@outlook.com>
2026-04-27 17:26:53 +0700

    unica: patches: rro: update for M55 firmware

--- unica/patches/rro/customize.sh
+++ unica/patches/rro/customize.sh
@@ -1,11 +1,11 @@
 if [[ $TARGET_FIRMWARE != $SOURCE_FIRMWARE ]]; then
     if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
-        DECODE_APK "product" "overlay/framework-res__a73xqxx__auto_generated_rro_product.apk"
+        DECODE_APK "product" "overlay/framework-res__m55xqxx__auto_generated_rro_product.apk"
 
         echo "Applying stock overlay configs"
-        rm -rf "$APKTOOL_DIR/product/overlay/framework-res__a73xqxx__auto_generated_rro_product.apk/res"
+        rm -rf "$APKTOOL_DIR/product/overlay/framework-res__m55xqxx__auto_generated_rro_product.apk/res"
         cp -a --preserve=all \
             "$SRC_DIR/target/$TARGET_CODENAME/overlay" \
-            "$APKTOOL_DIR/product/overlay/framework-res__a73xqxx__auto_generated_rro_product.apk/res"
+            "$APKTOOL_DIR/product/overlay/framework-res__m55xqxx__auto_generated_rro_product.apk/res"
     fi
 fi
