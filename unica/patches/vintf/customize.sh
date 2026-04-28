96aea240d30bee02dd68561c9aea80c8ab80b065
Tjari <zadigsm@outlook.com>
2026-04-28 12:05:20 +0700

    unica: patches: vintf: update variables

--- unica/patches/vintf/customize.sh
+++ unica/patches/vintf/customize.sh
@@ -3,7 +3,7 @@ _LOG() { if $DEBUG; then LOGW "$1"; else ABORT "$1"; fi }
 if [ -f "$SRC_DIR/target/$TARGET_CODENAME/vintf/compatibility_matrix.device.xml" ]; then
     LOG "- Adding /system/system/etc/vintf/compatibility_matrix.device.xml"
     EVAL "cp -a \"$SRC_DIR/target/$TARGET_CODENAME/vintf/compatibility_matrix.device.xml\" \"$WORK_DIR/system/system/etc/vintf/compatibility_matrix.device.xml\""
-elif [[ "$SOURCE_PLATFORM_SDK_VERSION" == "$TARGET_PLATFORM_SDK_VERSION" ]]; then
+elif [[ "$SOURCE_API_LEVEL" == "$TARGET_API_LEVEL" ]]; then
     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/vintf/compatibility_matrix.device.xml"
 else
     _LOG "File not found: $SRC_DIR/target/$TARGET_CODENAME/vintf/compatibility_matrix.device.xml"
@@ -12,7 +12,7 @@ fi
 if [ -f "$SRC_DIR/target/$TARGET_CODENAME/vintf/manifest.xml" ]; then
     LOG "- Adding /system/system/etc/vintf/manifest.xml"
     EVAL "cp -a \"$SRC_DIR/target/$TARGET_CODENAME/vintf/manifest.xml\" \"$WORK_DIR/system/system/etc/vintf/manifest.xml\""
-elif [[ "$SOURCE_PLATFORM_SDK_VERSION" == "$TARGET_PLATFORM_SDK_VERSION" ]]; then
+elif [[ "$SOURCE_API_LEVEL" == "$TARGET_API_LEVEL" ]]; then
     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/vintf/manifest.xml"
 fi
 
