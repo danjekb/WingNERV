89570344b2e6290aabd8bc776381474fee143035
danjekb <139584970+danjekb@users.noreply.github.com>
2026-06-01 08:45:56 +0200

    Delete unica/patches/jdm_cam/customize.sh

--- unica/patches/jdm_cam/customize.sh
+++
@@ -1,8 +0,0 @@
-TARGET_FIRMWARE_PATH="$(cut -d "/" -f 1 -s <<< "$TARGET_FIRMWARE")_$(cut -d "/" -f 2 -s <<< "$TARGET_FIRMWARE")"
-
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/SamSungCamera" 0 0 755 "u:object_r:system_file:s0"
-for f in $(find $FW_DIR/$TARGET_FIRMWARE_PATH/system/system/lib64 -printf "%P\\n" | grep -E 'lib(jni)*[Bb][Ss][Tt]'); do
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/$f" 0 0 644 "u:object_r:system_lib_file:s0"
-done
-
-unset TARGET_FIRMWARE_PATH
