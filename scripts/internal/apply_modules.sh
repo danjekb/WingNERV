cd28a9d6ebc6e7540fedc0125990eaecfe248091
danjekb <139584970+danjekb@users.noreply.github.com>
2026-06-08 02:13:12 +0200

    Refactor apply_modules.sh for improved readability

--- scripts/internal/apply_modules.sh
+++ scripts/internal/apply_modules.sh
@@ -1,22 +1,8 @@
 #!/usr/bin/env bash
-#
-# Copyright (C) 2023 Salvo Giangreco
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+# Copyright (c) 2023 Salvo Giangreco
+# SPDX-License-Identifier: GPL-3.0-or-later
 
-set -e
+set +e
 
 #[
 source "$SRC_DIR/scripts/utils/module_utils.sh" || exit 1
@@ -32,8 +18,8 @@ APPLY_MODULE()
         return 1
     fi
 
-    if [ -d "$MODPATH/$TARGET_SINGLE_SYSTEM_IMAGE" ]; then
-        MODPATH="$MODPATH/$TARGET_SINGLE_SYSTEM_IMAGE"
+    if [ -d "$MODPATH/$TARGET_OS_SINGLE_SYSTEM_IMAGE" ]; then
+        MODPATH="$MODPATH/$TARGET_OS_SINGLE_SYSTEM_IMAGE"
     fi
 
     if [ ! -f "$MODPATH/module.prop" ]; then
@@ -43,22 +29,34 @@ APPLY_MODULE()
         return 0
     else
         MODNAME="$(grep "^name" "$MODPATH/module.prop" | sed "s/name=//")"
-        MODAUTH="$(grep "^author" "$MODPATH/module.prop" | sed "s/author=//" | sed "s/, /, @/")"
+        MODAUTH="$(grep "^author" "$MODPATH/module.prop" | sed "s/author=//" | sed "s/, /, @/g")"
     fi
 
     LOG_STEP_IN "- Processing \"$MODNAME\" by @$MODAUTH"
 
     if ! grep -q "^SKIPUNZIP=1$" "$MODPATH/customize.sh" 2> /dev/null; then
-        [ -d "$MODPATH/odm" ] && ADD_TO_WORK_DIR "$MODPATH" "odm" "." 0 0 755 "u:object_r:vendor_file:s0"
-        [ -d "$MODPATH/product" ] && ADD_TO_WORK_DIR "$MODPATH" "product" "." 0 0 755 "u:object_r:system_file:s0"
-        [ -d "$MODPATH/system" ] && ADD_TO_WORK_DIR "$MODPATH" "system" "." 0 0 755 "u:object_r:system_file:s0"
-        [ -d "$MODPATH/system_ext" ] && ADD_TO_WORK_DIR "$MODPATH" "system_ext" "." 0 0 755 "u:object_r:system_file:s0"
-        [ -d "$MODPATH/vendor" ] && ADD_TO_WORK_DIR "$MODPATH" "vendor" "." 0 2000 755 "u:object_r:vendor_file:s0"
+        if [ -d "$MODPATH/odm" ]; then
+            ADD_TO_WORK_DIR "$MODPATH" "odm" "." 0 0 755 "u:object_r:vendor_file:s0"
+        fi
+        if [ -d "$MODPATH/product" ]; then
+            ADD_TO_WORK_DIR "$MODPATH" "product" "." 0 0 755 "u:object_r:system_file:s0"
+        fi
+        if [ -d "$MODPATH/system" ]; then
+            ADD_TO_WORK_DIR "$MODPATH" "system" "." 0 0 755 "u:object_r:system_file:s0"
+        fi
+        if [ -d "$MODPATH/system_ext" ]; then
+            ADD_TO_WORK_DIR "$MODPATH" "system_ext" "." 0 0 755 "u:object_r:system_file:s0"
+        fi
+        if [ -d "$MODPATH/vendor" ]; then
+            ADD_TO_WORK_DIR "$MODPATH" "vendor" "." 0 2000 755 "u:object_r:vendor_file:s0"
+        fi
     fi
 
     READ_AND_APPLY_PROPS "$MODPATH"
 
-    [ -f "$MODPATH/customize.sh" ] && . "$MODPATH/customize.sh"
+    if [ -f "$MODPATH/customize.sh" ]; then
+        . "$MODPATH/customize.sh"
+    fi
 
     if [ -d "$MODPATH/smali" ]; then
         while IFS= read -r f; do
@@ -86,15 +84,8 @@ APPLY_SMALI_PATCHES()
 
     while IFS= read -r p; do
         local FILE="$TARGET"
-        [[ "$PARTITION" != "system" ]] && FILE="$(cut -d "/" -f 2- -s <<< "$FILE")"
-
-        # TODO remove
-        if [[ "$p" == *"0000-"* ]]; then
-            if $ROM_IS_OFFICIAL; then
-                [[ "$p" == *"AOSP"* ]] && continue
-            else
-                [[ "$p" == *"UNICA"* ]] && continue
-            fi
+        if [[ "$PARTITION" != "system" ]]; then
+            FILE="$(cut -d "/" -f 2- -s <<< "$FILE")"
         fi
 
         APPLY_PATCH "$PARTITION" "$FILE" "$p"
@@ -113,8 +104,9 @@ READ_AND_APPLY_PROPS()
         IS_VALID_PARTITION_NAME "$PARTITION" || continue
 
         while read -r l; do
-            [[ "$l" == "#"* ]] && continue
-            [ ! "$l" ] && continue
+            if [[ "$l" == "#"* ]] || [ ! "$l" ]; then
+                continue
+            fi
 
             if grep -q -F "=" <<< "$l"; then
                 if [ ! "$(cut -d "=" -f 2- -s <<< "$l")" ]; then
@@ -142,7 +134,7 @@ elif [ ! -d "$1" ]; then
 fi
 
 while IFS= read -r f; do
-    APPLY_MODULE "$f"
-done < <(find "$1" -mindepth 1 -maxdepth 1 -type d | sort)
+    APPLY_MODULE "$f" || echo "WARNING: patch failed, skipping: ${f//$SRC_DIR\//}"
+done < <(find "$1" -mindepth 1 -maxdepth 1 -type d | LC_ALL=C sort)
 
 exit 0
